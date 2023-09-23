b3277c8d40f8041a4ba646e9377d9e1e6a41308e
Swarzstein <zorrillagja@gmail.com>
2023-09-23 04:46:00 -0400

    cors headers set

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ Greeting.create([
   { text: "Hello! this is the front end and I come from the Back end." },
   { text: "Hola! este es el front-end y vengo del back-end." },
   { text: "Hallo! dies ist das Frontend und ich komme vom Backend." },
-  { text: "هُلَ! ءَاشْتَاشَال فُرُنْتَانْدْ اِبَانْغُ دَالْ بَكَانْدْ." },
-  { text: "ھولا! ئېستې ئېس ئېل فرونت-ئېند ئى ۋېنگو دېل باك-ئېند." }
+  { text: ".هُلَ! ءَاشْتَا ءَاشْ ءَال فُرُنْتَانْدْ اِبَانْغُ دَالْ بَكَانْدْ" },
+  { text: ".ھولا! ئېستې ئېس ئېل فرونت-ئېند ئى ۋېنگو دېل باك-ئېند" }
   ])
