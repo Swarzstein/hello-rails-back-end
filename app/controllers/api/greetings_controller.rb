48d07c0e8a94fded64b44995b2b0d224ba324ccb
Swarzstein <zorrillagja@gmail.com>
2023-09-23 05:01:36 -0400

    Linters applied

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,15 +2,15 @@ class Api::GreetingsController < ApplicationController
   before_action :set_cors_headers, only: [:index]
 
   def index
-    greeting = Greeting.order("RANDOM()").first
+    greeting = Greeting.order('RANDOM()').first
     render json: greeting
   end
 
   private
 
   def set_cors_headers
-    headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-    headers["Access-Control-Allow-Methods"] = "GET"
-    headers["Access-Control-Allow-Headers"] = "Content-Type"
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
   end
 end
